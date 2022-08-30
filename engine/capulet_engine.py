0bd69c7d1fa1d8396878541d8ad38ef690eedb31
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2022-08-30 05:59:43 +0100

    refactor willoughby_engine.py

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -9,5 +9,5 @@ class CapuletEngine(Engine):
     current_mileage: int
     last_service_mileage: int
 
-    def engine_should_be_serviced(self):
+    def engine_should_be_serviced(self) -> bool:
         return self.current_mileage - self.last_service_mileage > 30000
