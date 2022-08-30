0a9bbabd235d9e060e8f25e2efcc49e85d422f94
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2022-08-30 05:54:29 +0100

    refactor capulet_engine.py

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,13 +1,13 @@
-from abc import ABC
+from dataclasses import dataclass
+from datetime import date
+from engine import Engine
 
-from car import Car
 
-
-class CapuletEngine(Car, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
-        self.current_mileage = current_mileage
-        self.last_service_mileage = last_service_mileage
+@dataclass
+class CapuletEngine(Engine):
+    last_service_date: date
+    current_mileage: int
+    last_service_mileage: int
 
     def engine_should_be_serviced(self):
         return self.current_mileage - self.last_service_mileage > 30000
