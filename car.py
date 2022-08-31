081f2c010e997595eed65487646a08d78af9569f
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2022-08-31 08:59:13 +0100

    update carfactory

--- car.py
+++ car.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from engine.engine import Engine
 from battery.battery import Battery
+from tire.tire import Tire
 from .serviceable import Serviceable
 
 
@@ -8,6 +9,7 @@ from .serviceable import Serviceable
 class Car(Serviceable):
     engine: Engine
     battery: Battery
+    tire: Tire
 
     def needs_service(self):
-        return self.battery.needs_service() or self.engine.needs_service()
+        return self.battery.needs_service() or self.engine.needs_service() or self.tire.needs_service()
--- car_factory.py
+++ car_factory.py
@@ -5,41 +5,48 @@ from engine.model.willoughby_engine import WilloughbyEngine
 from engine.model.sternman_engine import SternmanEngine
 from battery.model.spindler import SpindlerBattery
 from battery.model.nubbin import NubbinBattery
+from tire.model.carrigan import Carrigan
+from tire.model.octoprime import Octoprime
 
 
 class CarFactory:
 
     @staticmethod
-    def create_caliope(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+    def create_caliope(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tear_wear: list):
         caliopeEngine = CapuletEngine(
             last_service_date, current_mileage, last_service_mileage)
         caliopeBattery = SpindlerBattery(last_service_date, current_date)
-        return Car(caliopeEngine, caliopeBattery)
+        caliopeTire = Carrigan(tear_wear)
+        return Car(caliopeEngine, caliopeBattery, caliopeTire)
 
     @staticmethod
-    def create_glissade(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+    def create_glissade(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tear_wear: list):
         glissadeEngine = WilloughbyEngine(
             last_service_date, current_mileage, last_service_mileage)
         glissadeBattery = SpindlerBattery(last_service_date, current_date)
-        return Car(glissadeEngine, glissadeBattery)
+        glissadeTire = Carrigan(tear_wear)
+        return Car(glissadeEngine, glissadeBattery, glissadeTire)
 
     @staticmethod
-    def create_palindrome(current_date: date, last_service_date: date, warning_light_is_on: bool):
+    def create_palindrome(current_date: date, last_service_date: date, warning_light_is_on: bool, tear_wear: list):
         palindromeEngine = SternmanEngine(
             last_service_date, warning_light_is_on)
         palindromeBattery = SpindlerBattery(last_service_date, current_date)
-        return Car(palindromeEngine, palindromeBattery)
+        palidromeTire = Octoprime(tear_wear)
+        return Car(palindromeEngine, palindromeBattery, palidromeTire)
 
     @staticmethod
-    def create_rorschach(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+    def create_rorschach(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tear_wear: list):
         rorschachEngine = WilloughbyEngine(
             last_service_date, current_mileage, last_service_mileage)
         rorschachBattery = NubbinBattery(last_service_date, current_date)
-        return Car(rorschachEngine, rorschachBattery)
+        rorschachTire = Octoprime(tear_wear)
+        return Car(rorschachEngine, rorschachBattery, rorschachTire)
 
     @staticmethod
-    def create_thovex(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+    def create_thovex(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tear_wear: list):
         thovexEngine = CapuletEngine(
             last_service_date, current_mileage, last_service_mileage)
         thovexBattery = NubbinBattery(last_service_date, current_date)
-        return Car(thovexEngine, thovexBattery)
+        thovexTire = Carrigan(tear_wear)
+        return Car(thovexEngine, thovexBattery, thovexTire)
